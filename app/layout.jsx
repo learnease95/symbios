4d2c127aa6d927606b7e9b9d11018d6cb7e4991f
learnease95 <learnease95@gmail.com>
2024-06-16 06:02:59 -0700

    modified global css vertical extra white space

--- app/layout.jsx
+++ app/layout.jsx
@@ -13,8 +13,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Header />
-        {children}
+        <div className="box">
+          <Header />
+          {children}
+        </div>
       </body>
     </html>
   );
--- app/page.jsx
+++ app/page.jsx
@@ -3,44 +3,7 @@ import styles from "./page.module.css";
 export default function Home() {
   return (
     <div>
-      <h1>home page 1</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
-      <h1>home page</h1>
+      <p>Home page</p>
     </div>
   );
 }
